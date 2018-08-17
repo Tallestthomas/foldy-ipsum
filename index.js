0ba77f1fa271ef0283684bbc62b4a49f752eb66b
Thomas <trasmussen@redventures.com>
2018-08-17 08:37:37 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const terryIpsum = new GenerateNewText();
+
+function GenerateNewText(){
+  this.single = [
+    'flap',
+    'fold',
+    'hold',
+    'folds',
+    'holds',
+    'flaps',
+  ]
+
+  this.words = [
+    'foldy',
+    'holdy',
+    'flappy',
+  ];
+  this.beginning = [
+    'Grab my',
+    'Touch my',
+    'Hold my'
+  ]
+}
+
+GenerateNewText.prototype.getNewSentence = function(){
+  const beginning = this.beginning[Math.floor(Math.random() * this.beginning.length)];
+  const sentenceLength = Math.floor(Math.random() * Math.floor(3)) + 1;
+  let sentence = [beginning];
+
+  if(Math.round(Math.random()) === 1 || sentenceLength === 1 ){
+    sentence.push('terry');
+  }
+
+  if(sentenceLength > 1){
+    for(let i = 0; i < sentenceLength; i++){
+      let word = this.words[Math.floor(Math.random() * this.words.length)]
+      if(sentence.includes(word)){
+        break;
+      }
+      sentence.push(word);
+    }
+  }
+  sentence.push(this.single[Math.floor(Math.random() * this.single.length)]);
+  let newSentence = sentence.join(' ');
+  return newSentence + '.';
+}
+
+GenerateNewText.prototype.getParagraph = function(sentences = 5) {
+  let paragraph = [];
+  for(let i = 0; i < sentences; i++){
+    paragraph.push(this.getNewSentence());
+  }
+  return paragraph.join(' ');
+}
+
+GenerateNewText.prototype.getAllParagraphs = function(paragraphs = 2){
+
+}
+
+console.log(terryIpsum.getParagraph(2));
